e1090ea528b33c2933df58e2b6ebef658acc3130
Xantes Fabela <xantesfabela@Xantess-iMac.local>
2017-10-15 18:36:54 -0500

    added a collection view

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Xantes Fabela on 10/15/17.
+//  Copyright © 2017 Xantes Fabela. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTile: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
